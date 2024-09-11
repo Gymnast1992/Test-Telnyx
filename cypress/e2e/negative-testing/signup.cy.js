a963a68979a8e845d0da8876f010aa141927a773
Maksym Semiankiv <1992maksym1992@gmail.com>
2024-09-11 08:26:54 -0400

    10 TCs

--- cypress/e2e/negative-testing/signup.cy.js
+++ cypress/e2e/negative-testing/signup.cy.js
@@ -1,41 +1,41 @@
-describe('Positive testing', () => {
+describe('Negative testing', () => {
     beforeEach(() => {
       cy.visit('/sign-up')
     })
 
-    // it('TC05, The user should not be able to sign up with an empty field email', () => {
+    it('TC05, The user should not be able to sign up with an empty field email', () => {
         
-    //     const errorMessage = "errorThis field is required."
-    //     const firstName = "Maks"
-    //     const lastName = "Semen"
-    //     const password = "mynewTest123!"
+        const errorMessage = "errorThis field is required."
+        const firstName = "Maks"
+        const lastName = "Semen"
+        const password = "mynewTest123!"
         
-    //     cy.get('#first_name').type(firstName)
-    //     cy.get('#last_name').type(lastName)
-    //     cy.get('#password').type(password)
-    //     cy.get('#terms_and_conditions').check()
-    //     cy.get('[type="submit"]').eq(0).click()
-    //     cy.get('[id="email_message"]').should('have.text', errorMessage)
+        cy.get('#first_name').type(firstName)
+        cy.get('#last_name').type(lastName)
+        cy.get('#password').type(password)
+        cy.get('#terms_and_conditions').check()
+        cy.get('[type="submit"]').eq(0).click()
+        cy.get('[id="email_message"]').should('have.text', errorMessage)
 
-    // })
+    })
 
-    // it('TC06, The user should not be able to sign up without checkmark Terms and Conditions', () => {
+    it('TC06, The user should not be able to sign up without checkmark Terms and Conditions', () => {
 
-    //   const errorMessage = "errorPlease accept the terms and conditions"
-    //   const email = "testtelnyx@gmail.com"
-    //   const firstName = "Maksym"
-    //   const lastName = "Semen"
-    //   const password = "mynewTest123!"
+      const errorMessage = "errorPlease accept the terms and conditions"
+      const email = "testtelnyx@gmail.com"
+      const firstName = "Maksym"
+      const lastName = "Semen"
+      const password = "mynewTest123!"
         
-    //   cy.get('#email').type(email)
-    //   cy.get('#first_name').type(firstName)
-    //   cy.get('#last_name').type(lastName)
-    //   cy.get('#password').type(password)
+      cy.get('#email').type(email)
+      cy.get('#first_name').type(firstName)
+      cy.get('#last_name').type(lastName)
+      cy.get('#password').type(password)
       
-    //   cy.get('[type="submit"]').eq(0).click()
-    //   cy.get('#terms_and_conditions_message').should('have.text', errorMessage)
+      cy.get('[type="submit"]').eq(0).click()
+      cy.get('#terms_and_conditions_message').should('have.text', errorMessage)
 
-    // })
+    })
 
     it('TC07, The user should not be able to sign up with a password of less than 12 characters', () =>{
       
---
+++ cypress/e2e/positive-testing/shop.cy.js
@@ -0,0 +1,19 @@
+const text = "Featured products"
+
+describe('Positive testing', () => {
+  beforeEach(() => {
+    cy.visit('https://shop.telnyx.com/')
+  })
+
+  it('TC09, The user should be able to navigate to the Telnyx shop', () => {
+    cy.contains(text)
+      .should("be.visible")
+
+  })
+
+  it('TC10, The user should be able to navigate to the Accessories section and verify the card', () => {
+    cy.get('#HeaderDrawer-accessories').click({force: true})
+    cy.get('#CardLink-template--14828910739534__product-grid-6751953256526')
+      .should('be.visible')
+  })
+})
